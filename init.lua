9964604d2997a7802fe9c21b6c20aec8b29e3513
skylar-eng <mzs.112000@gmail.com>
2021-09-30 05:47:10 -0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,17 @@
+local modpath = minetest.get_modpath("terra_grande")
+
+---- Biomes ----
+
+minetest.register_biome({
+    name = 'luna',
+    node_top = 'default:silver_sand',
+    depth_top = 3,
+    node_filler = 'default:gravel',
+    depth_filler = 9,
+    node_stone = 'default:stone',
+    y_max = 255,
+    y_min = -255,
+    vertical_blend = 12,
+    heat_point = 30,
+    humidity_point = 90,
+})
