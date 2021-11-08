c8dfa19da6f4209723c10f7663a195602fd9dd33
skylar-eng <mzs.112000@gmail.com>
2021-11-08 09:18:54 -0800

    Update init.lua
    Updates biomes to hopefully fix the coal seam fire, and flood basalt biomes so they aren't quite as huge.
    Also updates luna biome to have silver sand spawn at the surface, so it's not as barren(this is still WIP, will need better values to make it less cringe).

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ local modpath = minetest.get_modpath("morebiomes")
 minetest.register_biome({
     name = 'luna',
     node_top = 'default:gravel',
-    depth_top = 3,
+    depth_top = 1,
     node_filler = 'default:silver_sand',
     depth_filler = 9,
     node_stone = 'default:stone',
@@ -14,38 +14,38 @@ minetest.register_biome({
     y_max = 255,
     y_min = -255,
     vertical_blend = 12,
-    heat_point = 30,
-    humidity_point = 90,
+    heat_point = 95,
+    humidity_point = 0,
 })
 
 
 minetest.register_biome({
     name = 'floodbasalt',
     node_top = 'default:lava_source',
-    depth_top = 6,
+    depth_top = 3,
     node_filler = 'default:obsidian',
     depth_filler = 1,
     node_stone = 'default:stone',
     node_water_top = 'default:obsidian',
     node_water = 'default:sand',
-    y_max = 49,
+    y_max = 6,
     y_min = -49,
     vertical_blend = 2,
-    heat_point = 100,
-    humidity_point = 15,
+    heat_point = 150,
+    humidity_point = 0,
 })
 
 
 minetest.register_biome({
     name = 'glassland',
     node_top = 'default:glass',
-    depth_top = 9,
-    node_filler = 'default:sand',
-    depth_filler = 10,
+    depth_top = 1,
+    node_filler = 'default:silver_sand',
+    depth_filler = 3,
     node_stone = 'default:stone',
     y_max = 255,
-    y_min = -255,
-    vertical_blend = 12,
+    y_min = -5,
+    vertical_blend = 0,
     heat_point = 50,
     humidity_point = 0,
 })
@@ -58,10 +58,10 @@ minetest.register_biome({
     node_filler = 'default:coalblock',
     depth_filler = 7,
     node_stone = 'default:stone_with_coal',
-    y_max = 48,
+    y_max = 6,
     y_min = -511,
     vertical_blend = 1,
-    heat_point = 85,
+    heat_point = 100,
     humidity_point = 50,
 })
 
@@ -93,26 +93,23 @@ minetest.register_ore({
 })
 
 
-minetest.register_biome({
-    name = 'bamboo_firest',
-    node_top = 'default:dirt_with_grass',
-    depth_top = 1,
-    node_filler = 'default:dirt',
-    depth_filler = 10,
-    node_stone = 'default:stone',
-    y_max = 64,
-    y_min = -20,
-    vertical_blend = 0,
-    heat_point = 40,
-    humidity_point = 100,
-})
-
 minetest.register_decoration({
     deco_type = 'simple',
-    place_on = {'default:dirt_with_grass'},
+    place_on = {'default:gravel'},
     sidelen = 4,
     fill_ratio = 0.02,
     noise_params = {offset=0, scale=.45, spread={x=100, y=100, z=100}, seed=354, octaves=3, persist=0.7},
-    biomes = {"bamboo_forest"},
-    decoration = "default:papyrus",
+    biomes = {"luna"},
+    decoration = "default:silver_sand",
+})
+
+
+minetest.register_decoration({
+    deco_type = 'simple',
+    place_on = {'default:lava_source'},
+    sidelen = 4,
+    fill_ratio = 0.02,
+    noise_params = {offset=0, scale=.45, spread={x=100, y=100, z=100}, seed=354, octaves=3, persist=0.7},
+    biomes = {"luna"},
+    decoration = "default:obsidian_slab",
 })
